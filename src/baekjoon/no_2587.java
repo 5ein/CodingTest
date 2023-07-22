4e9b771774984cb7a48d1bd2ae281bab88ec2810
sein Hwang <hhhj0525@daum.net>
2023-07-23 01:53:20 +0900

    feat: 7.23 문제풀기

---
+++ src/baekjoon/no_2587.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class no_2587 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[5];
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		br.close();
+		Arrays.sort(arr);
+		
+		System.out.println(sum / 5);
+		System.out.println(arr[2]);
+	}
+}
---
+++ src/baekjoon/no_2750.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class no_2750 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		Arrays.sort(arr);
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
