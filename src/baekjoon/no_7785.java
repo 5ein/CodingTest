c959de1f4bb734f32e414303bbcca1997c237f5f
sein Hwang <hhhj0525@daum.net>
2023-07-29 03:45:04 +0900

    feat: 7.29 문제풀기

---
+++ src/baekjoon/no_7785.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class no_7785 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		HashMap<String, String> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			String name = sc.next();
+			String state = sc.next();
+			
+			if(map.containsKey(name)) map.remove(name);
+			else map.put(name, state);
+		}
+		sc.close();
+		
+		ArrayList<String> list = new ArrayList<String>(map.keySet());
+		Collections.sort(list, Collections.reverseOrder());
+		
+		for (String s : list) {
+			System.out.println(s);
+		}
+		
+	}
+
+}
