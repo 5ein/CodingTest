cbc48e5a5fd741e4b73b3a2f32a5e894f9b07242
sein Hwang <hhhj0525@daum.net>
2023-09-10 18:17:07 +0900

    chore: 9.10 추가

--- src/Programmers/문자열여러번뒤집기.java
+++ src/Programmers/문자열여러번뒤집기.java
@@ -9,30 +9,47 @@ public class 문자열여러번뒤집기 {
 		
 		System.out.println(solution(my_string, queries));
 	}
-	
+        
     public static String solution(String my_string, int[][] queries) {
-        String answer = "";
-        
-        int len = my_string.length();
-        String[] my_str = my_string.split("");
-        String[] str = new String[len];
-        
-        for(int i = 0; i < queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            
-            for(int j = 0; j <= e-s; j++) {
-                str[e-j] = my_str[s+j];
-            }
-            for(int j = 0; j <= e-s; j++) {
-                my_str[s+j] = str[s+j];
-            }
-        }
-        
-        for(int i = 0; i < len; i++) {
-            answer += my_str[i];
-        }
-        
+    	String answer = "";
+    	StringBuilder sb = new StringBuilder(my_string);
+        for (int i = 0; i < queries.length; i++) {
+			int start = queries[i][0];
+			int end = queries[i][1];
+			
+			StringBuilder sb2 = new StringBuilder(sb.substring(start, end + 1));
+			sb2.reverse();
+			
+			sb.replace(start, end + 1, sb2.toString());
+		}
+        answer = sb.toString();
         return answer;
     }
+	
+//    public static String solution(String my_string, int[][] queries) {
+//        String answer = "";
+//        
+//        int len = my_string.length();
+//        String[] my_str = my_string.split("");
+//        String[] str = new String[len];
+//        
+//        for(int i = 0; i < queries.length; i++) {
+//            int s = queries[i][0];
+//            int e = queries[i][1];
+//            
+//            for(int j = 0; j <= e-s; j++) {
+//                str[e-j] = my_str[s+j];
+//            }
+//            for(int j = 0; j <= e-s; j++) {
+//                my_str[s+j] = str[s+j];
+//            }
+//        }
+//        
+//        for(int i = 0; i < len; i++) {
+//            answer += my_str[i];
+//        }
+//        
+//        return answer;
+//    }
+
 }
