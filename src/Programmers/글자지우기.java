b1c40c6baee222988cb0065027cb4862afa63980
sein Hwang <hhhj0525@daum.net>
2023-09-22 17:34:40 +0900

    feat: 9.22 문제풀기

---
+++ src/Programmers/글자지우기.java
@@ -0,0 +1,45 @@
+package programmers;
+
+public class 글자지우기 {
+	public static void main(String[] args) {
+		String my_string = "apporoograpemmemprs"; 
+		int[] indices = {1, 16, 6, 15, 0, 10, 11, 3};
+		System.out.println(solution(my_string, indices));
+		//programmers
+	}
+
+	//char
+    public static String solution(String my_string, int[] indices) {
+        String answer = "";
+        
+        char[] s = my_string.toCharArray();
+        for (int i : indices) {
+			s[i] = '0';
+		}
+        for(char i : s) {
+        	if(i != '0') {
+        		answer += i;
+        	}
+        }
+        
+        return answer;
+    }
+    
+    //String
+//    public static String solution(String my_string, int[] indices) {
+//    	String answer = "";
+//    	
+//        String[] tmp = my_string.split("");
+//
+//        for (int i = 0; i < indices.length; i++) {
+//            tmp[indices[i]] = "";
+//        }
+//
+//        for (String x : tmp) {
+//            answer += x;
+//        }
+//    	
+//    	return answer;
+//    }
+    
+}
