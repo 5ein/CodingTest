9ebad329da156f96023cc9c021ff4258a2db5b26
sein Hwang <hhhj0525@daum.net>
2023-09-26 00:23:50 +0900

    feat: 9.26 문제풀기

---
+++ src/Programmers/첫번째로냐오는음수.java
@@ -0,0 +1,24 @@
+package programmers;
+
+public class 첫번째로냐오는음수 {
+
+	public static void main(String[] args) {
+		int[] num_list = {12, 4, 15, 46, 38, -2, 15};
+		
+		System.out.println(solution(num_list));
+	}
+	
+    public static int solution(int[] num_list) {
+        int answer = -1;
+        
+        for (int i = 0; i < num_list.length; i++) {
+        	if(num_list[i] < 0) {
+        		answer = i;
+        		break;
+        	}
+		}
+        
+        return answer;
+    }
+
+}
