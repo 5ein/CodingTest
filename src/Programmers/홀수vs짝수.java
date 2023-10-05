73a33e1664471c7ff6477a43e8f0b859b6d60da3
sein Hwang <hhhj0525@daum.net>
2023-10-05 17:24:30 +0900

    feat: 10.5 문제풀기

---
+++ src/Programmers/홀수vs짝수.java
@@ -0,0 +1,52 @@
+package programmers;
+
+import java.io.Console;
+import java.util.Arrays;
+
+public class 홀수vs짝수 {
+
+	public static void main(String[] args) {
+		int[] num_list = {4, 2, 6, 1, 7, 6};
+		
+		System.out.println(solution(num_list));
+	}
+	
+//    public static int solution(int[] num_list) {
+//        int answer = 0;
+//        int one = 0;
+//        int two = 0;
+//        for (int i = 0; i < num_list.length; i++) {
+//			if(i % 2 == 0) {
+//				two += num_list[i];
+//			}else {
+//				one += num_list[i];
+//			}
+//		}
+//        
+//        if(one >= two) {
+//        	answer = one;
+//        }else {
+//        	answer = two;
+//        }
+//        
+//        return answer;
+//    }
+    
+    public static int solution(int[] num_list) {
+    	int answer = 0;
+    	int one = 0;
+    	int two = 0;
+    	for (int i = 0; i < num_list.length; i++) {
+    		if(i % 2 == 0) {
+    			two += num_list[i];
+    		}else {
+    			one += num_list[i];
+    		}
+    	}
+    	
+    	answer = one > two ? one : two;
+    	
+    	return answer;
+    }
+
+}
