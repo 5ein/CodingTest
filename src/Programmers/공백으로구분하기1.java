8d5252684cc5deb3d8798caa1261d07e503d2513
sein Hwang <hhhj0525@daum.net>
2023-10-23 03:54:24 +0900

    feat: 10.23 문제풀기

---
+++ src/Programmers/공백으로구분하기1.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 공백으로구분하기1 {
+
+	public static void main(String[] args) {
+		String my_string = "i love you";
+		System.out.println(Arrays.toString(solution(my_string)));
+	}
+	
+    public static String[] solution(String my_string) {
+        String[] answer = {};
+        
+        answer = my_string.split(" ");
+        
+        return answer;
+    }
+
+}
