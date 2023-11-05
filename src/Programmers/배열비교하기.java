0d39007a3dfa3955ad6b3f734c6b22ba4bb7f5fe
sein Hwang <hhhj0525@daum.net>
2023-11-05 22:51:55 +0900

    feat: 11.5  문제풀기

---
+++ src/Programmers/배열비교하기.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class 배열비교하기 {
+
+	public static void main(String[] args) {
+		int[] arr1 = {100, 17, 84, 1};
+		int[] arr2 = {55, 12, 65, 36};
+		System.out.println(solution(arr1, arr2));
+	}
+    public static int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        
+        
+        return answer;
+    }
+
+}
