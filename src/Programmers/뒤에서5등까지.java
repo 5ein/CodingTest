5b00590e28722ba7db38c82298c66ea7d68d6e74
sein Hwang <hhhj0525@daum.net>
2023-11-08 23:02:47 +0900

    feat: 11.8 문제풀기

---
+++ src/Programmers/뒤에서5등까지.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class 뒤에서5등까지 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+        return answer;
+    }
+
+}
