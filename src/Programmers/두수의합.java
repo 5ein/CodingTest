c746e2509401594f76681e82a464f3aec7baee30
sein Hwang <hhhj0525@daum.net>
2023-11-15 21:55:52 +0900

    feat:11.15 문제풀기

--- src/Programmers/두수의합.java
+++ src/Programmers/두수의합.java
@@ -1,14 +1,35 @@
 package programmers;
 
+import java.math.BigInteger;
+
 public class 두수의합 {
 
 	public static void main(String[] args) {
-		int num1 = 2;
-		int num2 = 3;
+		String a = "582";
+		String b = "734";
 
-		int answer = num1 + num2;
-		
-		System.out.println(answer);
+		System.out.println(solution(a, b));
 	}
+	
+    public static String solution(String a, String b) {
+        String answer = "";
+        BigInteger A = new BigInteger(a);
+        BigInteger B = new BigInteger(b);
+        BigInteger sum = A.add(B);
+        answer = sum.toString();
+        return answer;
+    }
+    
+    //다른사람 풀이
+//    public static String solution(String a, String b) {
+//        String answer = "";
+//
+//        BigInteger A = new BigInteger(a);
+//        BigInteger B = new BigInteger(b);
+//
+//        answer += A.add(B); //이렇게 스트링으로 바로 바뀌게 더해줬음
+//
+//        return answer;
+//    }
 
 }
