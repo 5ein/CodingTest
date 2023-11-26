d9d5d77995ed4ad9f6518bd841b00cbcc083bcad
sein Hwang <hhhj0525@daum.net>
2023-11-26 20:39:59 +0900

    feat: 11.26 문제풀기

--- src/Programmers/조건에맞게수열변환하기3.java
+++ src/Programmers/조건에맞게수열변환하기3.java
@@ -4,21 +4,44 @@ import java.util.Arrays;
 
 public class 조건에맞게수열변환하기3 {
 
+//	public static void main(String[] args) {
+//		int[] arr = {1, 2, 3, 100, 99, 98};
+//		int k = 3;
+//
+//		int[] answer = new int[arr.length];
+//		
+//		for (int i = 0; i < arr.length; i++) {
+//			if(k % 2 == 0) {
+//				answer[i] = arr[i] + k;
+//			}else if (k % 2 >0) {
+//				answer[i] = arr[i] * k;
+//			}
+//		}
+//		
+//		System.out.println(Arrays.toString(answer));
+//	}
+	
 	public static void main(String[] args) {
 		int[] arr = {1, 2, 3, 100, 99, 98};
 		int k = 3;
-
-		int[] answer = new int[arr.length];
-		
-		for (int i = 0; i < arr.length; i++) {
-			if(k % 2 == 0) {
-				answer[i] = arr[i] + k;
-			}else if (k % 2 >0) {
-				answer[i] = arr[i] * k;
-			}
-		}
-		
-		System.out.println(Arrays.toString(answer));
+		System.out.println(Arrays.toString(solution(arr, k)));
 	}
+	
+    public static int[] solution(int[] arr, int k) {
+        int[] answer = {};
+        
+        answer = arr.clone();
+        if(k % 2 == 0) {
+        	for (int i = 0; i < answer.length; i++) {
+				answer[i] += k;
+			}
+        } else {
+        	for (int i = 0; i < answer.length; i++) {
+        		answer[i] *= k;
+        	}
+        }
+        
+        return answer;
+    }
 
 }
