bbbd11512e383f044d3c0bde74548291953a256e
sein Hwang <hhhj0525@daum.net>
2023-12-22 07:35:52 +0900

    feat: 12.22 문제풀기

---
+++ src/Programmers/문자열안에문자열.java
@@ -0,0 +1,18 @@
+package programmers;
+
+public class 문자열안에문자열 {
+
+	public static void main(String[] args) {
+		String str1 = "ab6CDE443fgh22iJKlmn1o";
+		String str2 = "6CD";
+				
+		System.out.println(solution(str1, str2));		
+	}
+	
+    public static int solution(String str1, String str2) {
+        int answer = 0;
+        answer = str1.contains(str2)? 1 : 2;
+        return answer;
+    }
+
+}
