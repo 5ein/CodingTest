004854dd4ec8a596ed266b0910b67af8241de8af
sein Hwang <hhhj0525@daum.net>
2024-01-18 20:35:29 +0900

    feat: 1.18 문제풀기

---
+++ src/Programmers/PCCE기출문제1.java
@@ -0,0 +1,19 @@
+package programmers;
+
+public class PCCE기출문제1 {
+	
+	//출력 예시
+//	Spring is beginning
+//	13
+//	310
+	public static void main(String[] args) {
+       String msg = "Spring is beginning";
+       int val1 = 3;
+       String val2 = "3";
+
+       System.out.println(msg);
+       System.out.println(val1 + 10);
+       System.out.println(val2 + "10");
+	}
+
+}
