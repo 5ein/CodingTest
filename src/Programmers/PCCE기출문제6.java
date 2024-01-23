8ab1d739f5128bbf1569ec3860a5ba23fbc1a41d
sein Hwang <hhhj0525@daum.net>
2024-01-23 15:11:23 +0900

    feat: 1.23 문제풀기

---
+++ src/Programmers/PCCE기출문제6.java
@@ -0,0 +1,30 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class PCCE기출문제6 {
+
+	public static void main(String[] args) {
+		int[] numbers = {3, 4};
+		int[] our_score = {85, 93}; 
+		int[] score_list = {85, 92, 38, 93, 48, 85, 92, 56};
+		System.out.println(Arrays.toString(solution(numbers, our_score, score_list)));
+	}
+	
+	public static String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+        int num_student = numbers.length;
+        String[] answer = new String[num_student];
+
+        for (int i = 0; i < num_student; i++) {
+            if (our_score[i] == score_list[numbers[i] - 1]) {
+                answer[i] = "Same";
+            }
+            else {
+                answer[i] = "Different";
+            }
+        }
+
+        return answer;
+    }
+
+}
