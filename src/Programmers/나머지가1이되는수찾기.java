7e4d0212a9634f35e90dcd71322e990669610df3
sein Hwang <hhhj0525@daum.net>
2024-01-28 23:19:34 +0900

    feat: 1.28 문제풀기

---
+++ src/Programmers/나머지가1이되는수찾기.java
@@ -0,0 +1,33 @@
+package programmers;
+
+public class 나머지가1이되는수찾기 {
+
+	public static void main(String[] args) {
+		int n = 10;
+		
+		System.out.println(solution(n));
+	}
+	
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 2; i < n; i++) {
+			if(n % i  == 1) {
+				answer = i;
+				break;
+			}
+		}
+        return answer;
+    }
+    
+//    public int solution(int n) {
+//        int answer = 1;
+//
+//        while(true) {
+//            if (n%answer==1) break;
+//            answer++;
+//        }
+//
+//        return answer;
+//    }
+
+}
