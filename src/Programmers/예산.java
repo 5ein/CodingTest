005395103cee49b026cf9049c721ee546eec13d0
sein Hwang <hhhj0525@daum.net>
2024-02-19 03:33:45 +0900

    feat: 2.19 문제풀기

---
+++ src/Programmers/예산.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 예산 {
+
+	public static void main(String[] args) {
+		int[] d = {1,3,2,5,4};
+		int budget = 9;
+		System.out.println(solution(d, budget));
+	}
+	
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        for (int i : d) {
+        	budget -= i;
+        	if(budget < 0) break;
+        	answer ++;
+		}
+        return answer;
+    }
+
+}
