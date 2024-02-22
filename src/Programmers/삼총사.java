0a61ecd9a7509a4eb459da5774c11b6327f019ba
sein Hwang <hhhj0525@daum.net>
2024-02-22 23:40:19 +0900

    feat: 2.22 문제풀기

---
+++ src/Programmers/삼총사.java
@@ -0,0 +1,34 @@
+package programmers;
+
+public class 삼총사 {
+
+	public static void main(String[] args) {
+		int[] number = {-2, 3, 0, 2, -5};
+		System.out.println(solution(number));
+	}
+	
+//    public static int solution(int[] number) {
+//        int answer = 0;
+//        for (int i = 0; i < number.length; i++) {
+//			for (int j = i + 1; j < number.length; j++) {
+//				for (int k = j + 1; k < number.length; k++) {
+//					if(number[i] + number[j] + number[k] == 0) answer ++;
+//				}
+//			}
+//		}
+//        return answer;
+//    }
+    
+    public static int solution(int[] number) {
+    	int answer = 0;
+    	for (int i = 0; i < number.length - 2; i++) {
+    		for (int j = i + 1; j < number.length - 1; j++) {
+    			for (int k = j + 1; k < number.length; k++) {
+    				if(number[i] + number[j] + number[k] == 0) answer ++;
+    			}
+    		}
+    	}
+    	return answer;
+    }
+
+}
