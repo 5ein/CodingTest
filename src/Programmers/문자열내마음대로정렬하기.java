ab18b9da46449df98ba97e63a811aefaff9caccf
sein Hwang <hhhj0525@daum.net>
2024-02-28 00:39:08 +0900

    refactor: 2.27 코드수정

--- src/Programmers/문자열내마음대로정렬하기.java
+++ src/Programmers/문자열내마음대로정렬하기.java
@@ -1,59 +1,79 @@
 package programmers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class 문자열내마음대로정렬하기 {
 
 	public static void main(String[] args) {
-		String[] strings = {"sun", "bed", "car"};
-		int n = 1;
+//		String[] strings = {"sun", "bed", "car"};
+//		int n = 1;
+		String[] strings = {"abce", "abcd", "cdx"};
+		int n = 2;
 		System.out.println(Arrays.toString(solution(strings, n)));
 	}
 	
-    public static String[] solution(String[] strings, int n) {
-        Arrays.sort(strings);
+//	public static String[] solution(String[] strings, int n) {
+//		Arrays.sort(strings);
+//		String[] answer = new String[strings.length];
+//		char[] arr = new char[strings.length];
+//		for (int i = 0; i < strings.length; i++) {
+//			arr[i] = strings[i].charAt(n);
+//		}
+//		Arrays.sort(arr);
+//		List<String> list = new ArrayList<>(Arrays.asList(strings));
+//		for (int i = 0; i < answer.length; i++) {
+//			for (int j = 0; j < list.size(); j++) {
+//				if (list.get(j).charAt(n) == arr[i]) {
+//					answer[i] = list.get(j);
+//					list.remove(list.get(j));
+//					break;
+//				}
+//			}
+//		}
+//		return answer;
+//	}
+	
+	//좋은 풀이 같음
+	public static String[] solution(String[] strings, int n) {
         String[] answer = new String[strings.length];
         
-        for(int i = 0; i < strings.length; i++) {
-            answer[i] = strings[i].substring(n, n+1);
+        // 각 문자열의 n번째 문자를 맨 앞에 붙여서 저장
+        for (int i = 0; i < strings.length; i++) {
+            answer[i] = strings[i].charAt(n) + strings[i];
         }
+        
+        // 문자열 배열을 정렬
         Arrays.sort(answer);
         
-        List<String> list = new ArrayList<>(Arrays.asList(strings));
-        for(int i = 0; i < answer.length; i++) {
-            for(int j = 0; j < list.size(); j++) {
-                if(list.get(j).substring(n, n+1).equals(answer[i])) {
-                    answer[i] = list.get(j);
-                    list.remove(list.get(j));
-                    break;
-                }
-            }
+        // 정렬된 문자열 배열에서 n번째 문자를 제거하고 반환
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = answer[i].substring(1);
         }
         
         return answer;
-    }
-    
-    //왜 실패 ㅠㅠ
-//  public static String[] solution(String[] strings, int n) {
-//  String[] answer = new String[strings.length];
-//  Arrays.sort(strings);
-//  char[] N = new char[strings.length];
-//  for (int i = 0; i < N.length; i++) {
-//  	N[i] = strings[i].charAt(n);
-//	}
-//  Arrays.sort(N);
-//  for (int i = 0; i < N.length; i++) {
-//  	for (int j = 0; j < strings.length; j++) {
-//  		if(N[i] == strings[j].charAt(n)) {
-//  			answer[i] = strings[j];
-//  			strings[j] = "";
-//  			break;
-//  		}
-//		}
-//	}
-//  return answer;
-//}
+	}
+	
+	// 다른사람 풀이
+//    public static String[] solution(String[] strings, int n) {
+//        Arrays.sort(strings);
+//        String[] answer = new String[strings.length];
+//        for(int i = 0; i < strings.length; i++) {
+//            answer[i] = strings[i].substring(n, n+1);
+//        }
+//        Arrays.sort(answer);
+//        
+//        List<String> list = new ArrayList<>(Arrays.asList(strings));
+//        for(int i = 0; i < answer.length; i++) {
+//            for(int j = 0; j < list.size(); j++) {
+//                if(list.get(j).substring(n, n+1).equals(answer[i])) {
+//                    answer[i] = list.get(j);
+//                    list.remove(list.get(j));
+//                    break;
+//                }
+//            }
+//        }
+//        
+//        return answer;
+//    }
     
 }
