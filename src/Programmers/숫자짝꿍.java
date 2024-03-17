df58f9aa223b2183768a27c56898fd0496f8c481
sein Hwang <hhhj0525@daum.net>
2024-03-17 22:05:54 +0900

    feat: 3.17 문제풀기

---
+++ src/Programmers/숫자짝꿍.java
@@ -0,0 +1,49 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class 숫자짝꿍 {
+
+	public static void main(String[] args) {
+		String X = "5525";
+		String Y = "1255";
+		
+		System.out.println(solution(X, Y));
+	}
+	
+    public static String solution(String X, String Y) {
+        HashMap<Integer, Integer> xmap = new HashMap<>();
+        HashMap<Integer, Integer> ymap = new HashMap<>();
+        
+        for(String data : X.split("")){
+            int cur = Integer.parseInt(data);
+            xmap.put(cur, xmap.getOrDefault(cur, 0) + 1);
+        }
+
+        for(String data : Y.split("")){
+            int cur = Integer.parseInt(data);
+            ymap.put(cur, ymap.getOrDefault(cur, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 9; i >= 0 ; i --){
+            if(xmap.containsKey(i) && ymap.containsKey(i)){
+                int cnt = Math.min(xmap.get(i), ymap.get(i));
+                for (int j = 0; j < cnt; j++) {
+                    sb.append(i);
+                }
+            }
+        }
+
+        if(sb.toString().startsWith("0")){
+            return "0";
+        }
+        else if(sb.toString().equals("")){
+            return "-1";
+        }
+
+        return sb.toString();
+    }
+
+}
