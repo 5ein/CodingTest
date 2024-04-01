cdc7687fcd634ec3825db6a951c46ce57187823f
sein Hwang <hhhj0525@daum.net>
2024-04-01 22:10:12 +0900

    feat: 4.1 문제풀기

---
+++ src/Programmers/가장많이받는선물.java
@@ -0,0 +1,54 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class 가장많이받는선물 {
+
+	public static void main(String[] args) {
+		String[] friends = { "muzi", "ryan", "frodo", "neo" };
+		String[] gifts = { "muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi",
+				"frodo ryan", "neo muzi" };
+		System.out.println(solution(friends, gifts));
+	}
+
+	public static int solution(String[] friends, String[] gifts) {
+        int answer = 0;
+        int friendsLenght = friends.length;
+        HashMap<String, Integer> dic = new HashMap<>();
+        int[] giftDegree = new int[friendsLenght];
+        int[][] giftGraph = new int[friendsLenght][friendsLenght];
+        
+        for ( int i = 0; i < friendsLenght; i++ ) {
+            dic.put(friends[i], i);
+        }
+        
+        for ( String gift : gifts ) {
+            String[] giftName = gift.split(" ");
+            giftDegree[dic.get(giftName[0])]++;
+            giftDegree[dic.get(giftName[1])]--;
+            giftGraph[dic.get(giftName[0])][dic.get(giftName[1])]++;    
+        }
+        
+        for ( int i =0; i< friendsLenght; i++) {
+            int num = 0;
+            for ( int j = 0; j< friendsLenght; j++) {
+                if ( i == j) {
+                    continue;
+                }    
+                
+                if (giftGraph[i][j] > giftGraph[j][i] ||
+                     (giftGraph[i][j] == giftGraph[j][i] && giftDegree[i] > giftDegree[j])) {
+                        num++;
+                    }
+            }
+            
+            if ( answer < num) {
+                answer = num;
+            }
+            
+        }
+        
+        return answer;
+    }
+
+}
