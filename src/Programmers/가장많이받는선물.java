6629c2652552e0cc5051eee051a315c6995bd13a
sein Hwang <hhhj0525@daum.net>
2024-04-03 00:32:53 +0900

    refactor: 4.3 수정

--- src/Programmers/가장많이받는선물.java
+++ src/Programmers/가장많이받는선물.java
@@ -12,43 +12,68 @@ public class 가장많이받는선물 {
 	}
 
 	public static int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        int friendsLenght = friends.length;
-        HashMap<String, Integer> dic = new HashMap<>();
-        int[] giftDegree = new int[friendsLenght];
-        int[][] giftGraph = new int[friendsLenght][friendsLenght];
-        
-        for ( int i = 0; i < friendsLenght; i++ ) {
-            dic.put(friends[i], i);
-        }
-        
-        for ( String gift : gifts ) {
-            String[] giftName = gift.split(" ");
-            giftDegree[dic.get(giftName[0])]++;
-            giftDegree[dic.get(giftName[1])]--;
-            giftGraph[dic.get(giftName[0])][dic.get(giftName[1])]++;    
-        }
-        
-        for ( int i =0; i< friendsLenght; i++) {
-            int num = 0;
-            for ( int j = 0; j< friendsLenght; j++) {
-                if ( i == j) {
-                    continue;
-                }    
-                
-                if (giftGraph[i][j] > giftGraph[j][i] ||
-                     (giftGraph[i][j] == giftGraph[j][i] && giftDegree[i] > giftDegree[j])) {
-                        num++;
-                    }
-            }
-            
-            if ( answer < num) {
-                answer = num;
-            }
-            
-        }
-        
-        return answer;
-    }
+		int answer = 0;
+		int friendsLenght = friends.length;
+		HashMap<String, Integer> dic = new HashMap<>();
+		int[] giftDegree = new int[friendsLenght];
+		int[][] giftGraph = new int[friendsLenght][friendsLenght];
+
+		for (int i = 0; i < friendsLenght; i++) {
+			dic.put(friends[i], i);
+		}
+
+		for (String gift : gifts) {
+			String[] giftName = gift.split(" ");
+			giftDegree[dic.get(giftName[0])]++;
+			giftDegree[dic.get(giftName[1])]--;
+			giftGraph[dic.get(giftName[0])][dic.get(giftName[1])]++;
+		}
+
+		for (int i = 0; i < friendsLenght; i++) {
+			int num = 0;
+			for (int j = 0; j < friendsLenght; j++) {
+				if (i == j) continue;
+
+				if (giftGraph[i][j] > giftGraph[j][i]
+						|| (giftGraph[i][j] == giftGraph[j][i] 
+								&& giftDegree[i] > giftDegree[j])) {
+					num++;
+				}
+			}//for
+
+			if (answer < num) answer = num;
+		}
+
+		return answer;
+	}
+	
+	//다른 사람 풀이1
+//	public static int solution(String[] friends, String[] gifts) {
+//        Map<String, Integer> map = new HashMap<>();
+//        for (int i = 0; i < friends.length; i++) {
+//            map.put(friends[i], i);
+//        }
+//        int[] index = new int[friends.length];
+//        int[][] record = new int[friends.length][friends.length];
+//
+//        for (String str : gifts) {
+//            String[] cur = str.split(" ");
+//            index[map.get(cur[0])]++;
+//            index[map.get(cur[1])]--;
+//            record[map.get(cur[0])][map.get(cur[1])]++;
+//        }
+//
+//       int ans = 0;
+//       for (int i = 0; i < friends.length; i++) {
+//           int cnt = 0;
+//           for (int j = 0; j < friends.length; j++) {
+//               if(i == j) continue;
+//               if (record[i][j] > record[j][i]) cnt++;
+//               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
+//           }
+//           ans = Math.max(cnt, ans);
+//       }
+//        return ans;
+//    }
 
 }
--- src/Programmers/공원산책.java
+++ src/Programmers/공원산책.java
@@ -22,8 +22,8 @@ public class 공원산책 {
 			if (park[i].contains("S")) {
 				sy = i;
 				sx = park[i].indexOf("S");
-			}
-		}
+			}//if
+		}//for
 
 		for (String st : routes) {
 			String way = st.split(" ")[0];
@@ -35,31 +35,132 @@ public class 공원산책 {
 			for (int i = 0; i < len; i++) {
 				if (way.equals("E")) {
 					nx++;
-				}
+				}//if
 				if (way.equals("W")) {
 					nx--;
-				}
+				}//if
 				if (way.equals("S")) {
 					ny++;
-				}
+				}//if
 				if (way.equals("N")) {
 					ny--;
-				}
+				}//if
 				if (nx >= 0 && ny >= 0 && ny < arr.length && nx < arr[0].length) {
 					if (arr[ny][nx] == 'X') {
 						break;
-					}
-					// 범위내 & 장애물 x
-					if (i == len - 1) {
+					}//if
+					if (i == len - 1) { // 범위내 & 장애물 x
 						sx = nx;
 						sy = ny;
-					}
-				}
-			}
-		}
+					}//if
+				}//if
+			}//for
+		}//for
 
 		int[] answer = { sy, sx };
 		return answer;
 	}
-
+	
+	//다른사람풀이
+//	public static int[] solution(String[] park, String[] routes) {
+//        int h = park.length;
+//        int w = park[0].length();
+//
+//        int startH = 0;
+//        int startW = 0;
+//
+//        for(int i=0; i<h; i++){
+//            if(park[i].contains("S"))
+//            {
+//                startH = i;
+//                startW = park[i].indexOf("S"); 
+//            }
+//        }
+//
+//        for(String route : routes){
+//            String d = route.split(" ")[0];
+//            int move = Integer.parseInt(route.split(" ")[1]);
+//
+//            int moveH = startH;
+//            int moveW = startW;
+//
+//            for(int i=0; i<move; i++)
+//            {
+//                switch(d) {
+//                    case "S" : moveH++; break; 
+//                    case "N" : moveH--; break;
+//                    case "E" : moveW++; break;
+//                    case "W" : moveW--; break;
+//                }
+//            if(moveH>=0&& moveH<h&&moveW>=0&&moveW<w){
+//                if(park[moveH].substring(moveW,moveW+1).equals("X")){
+//                    break;
+//                }
+//                if(i==move-1){
+//                    startH=moveH;
+//                    startW=moveW;
+//                }
+//                }    
+//            }
+//
+//        }
+//
+//
+//        int[] answer = {startH,startW};
+//        return answer;
+//    }
+	
+	//다른사람 풀이2
+//	public static int[] solution(String[] park, String[] routes) {
+//        int m = park.length;
+//        int n = park[0].length();
+//        int curX = 0;
+//        int curY = 0;
+//        char[][] grid = new char[m][n];
+//
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                grid[i][j] = park[i].charAt(j);
+//                if (grid[i][j] == 'S') {
+//                    curX = i;
+//                    curY = j;
+//                }
+//            }
+//        }
+//
+//        int[] dx = { -1, 1, 0, 0 };
+//        int[] dy = { 0, 0, -1, 1 };
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        map.put('N', 0);
+//        map.put('S', 1);
+//        map.put('W', 2);
+//        map.put('E', 3);
+//
+//        for (String command : routes) {
+//
+//            char op = command.charAt(0);
+//            int move = command.charAt(2) - '0';
+//
+//            int nextX = curX;
+//            int nextY = curY;
+//            boolean isOK = true;
+//            for (int i = 0; i < move; i++) {
+//                nextX += dx[map.get(op)];
+//                nextY += dy[map.get(op)];
+//
+//                if (nextX < 0 || nextY < 0 || nextX >= m || nextY >= n || grid[nextX][nextY] == 'X') {
+//                    isOK = false;
+//                    break;
+//                }
+//            }
+//
+//            if (isOK) {
+//                curX = nextX;
+//                curY = nextY;
+//            }
+//        }
+//
+//        return new int[] { curX, curY };
+//    }
+	
 }
