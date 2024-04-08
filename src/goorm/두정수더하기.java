26bc38a7928e76849bcb80b707f83fa63c2ab885
sein Hwang <hhhj0525@daum.net>
2024-04-08 21:40:08 +0900

    feat: 4.8 문제풀기

---
+++ src/goorm/두정수더하기.java
@@ -0,0 +1,14 @@
+package goorm;
+
+import java.util.Scanner;
+
+public class 두정수더하기 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		sc.close();
+		
+		System.out.println(A + B);
+	}
+}
