1a9a4c36b37c2b0d158a693abe46f2af02bbbc71
sein Hwang <hhhj0525@daum.net>
2024-04-18 22:46:31 +0900

    feat: 4.18 문제풀기

---
+++ src/goorm/숫자제거정렬.java
@@ -0,0 +1,55 @@
+package goorm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+public class 숫자제거정렬 {
+	
+//	public static void main(String[] args) throws Exception {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int N = Integer.parseInt(br.readLine());
+//		Set<Integer> set = new HashSet<>();
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+//		for (int i = 0; i < N; i++) {
+//			set.add(Integer.parseInt(st.nextToken()));
+//		}
+//		br.close();
+//		int[] arr = new int[set.size()];
+//		int idx = 0;
+//		for (Integer i : set) {
+//			arr[idx] = i;
+//			idx++;
+//		}
+//		Arrays.sort(arr);
+//		String answer = "";
+//		for (int i : arr) {
+//			answer += i + " ";
+//		}
+//		System.out.println(answer);
+//	}
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> list = new ArrayList<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			if(!list.contains(n)) list.add(n);
+		}
+		br.close();
+		int[] arr = new int[list.size()];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = list.get(i);
+		}
+		Arrays.sort(arr);
+		String answer = "";
+		for (int i : arr) {
+			answer += i + " ";
+		}
+		System.out.println(answer);
+	}
+}
